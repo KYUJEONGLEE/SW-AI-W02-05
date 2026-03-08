ea82554b391fd201adaec258e3b6f71f7a1e644f
이규정 <veracro97@gmail.com>
2026-03-08 20:17:37 +0900

    solve 문자열_반복]

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,20 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+import sys
+
+T = int(sys.stdin.readline())
+S_list = [list(sys.stdin.readline().split()) for _ in range(T)]
+
+
+def repeat_string(input):
+    for S in input:
+        repeat_count = int(S[0])
+        repeat_str = S[1]
+
+        for s in repeat_str:
+            print(s * repeat_count, end="")
+        print("")
+
+
+repeat_string(S_list)
