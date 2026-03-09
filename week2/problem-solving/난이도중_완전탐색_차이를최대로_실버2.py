2c9cef036079f120fd55cb1cfc2daa47811d1025
이규정 <veracro97@gmail.com>
2026-03-09 17:41:44 +0900

    차이를 최대로 - nonlocal 변수, 각 순열의 abs 차 덧셈의 값을 리스트에 저장 X

--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -14,18 +14,24 @@ X = list(map(int, sys.stdin.readline().split()))
 # 나올 수 있는 조합의 개수 8!
 
 # 이것도 백트래킹으로 풀수 있지 않을까?
-#
 
 
 def perm(n):
     length_n = len(n)
-    result = []
+    max_val = 0
 
     def backtracking(current_perm, visited, sum_of_val):
         # 종료 조건 : 현재의 조합의 길이가 n의 길이와 같은 경우
         if len(current_perm) == length_n:
-            result_perm = current_perm.copy()
-            result.append(result_perm)
+            # 순열 리스트 대신 절대값들의 합을 저장함
+
+            # 새로 배운거 nonlocal : 함수 바로 밖에서 선언된 변수에 대해서
+            # 안쪽 함수에서 수정 가능하게 할 수 있도록 하는것
+            nonlocal max_val
+
+            if max_val < sum_of_val:
+                max_val = sum_of_val
+
             return
 
         for i in range(length_n):
@@ -33,14 +39,26 @@ def perm(n):
             if not visited[i]:
                 visited[i] = True
                 current_perm.append(n[i])
+
+                # 순열안에 들어있는 원소의 개수가 1개 이하라면 절대값 계산 불가
+                if len(current_perm) <= 1:
+                    abs_sub = 0
+
+                # 순열안에 2개 이상의 원소가 들어있을때부터 절대값 차이 계산
+                elif len(current_perm) > 1:
+                    # 끝에서 2개의 원소를 뺼셈
+                    abs_sub = abs(current_perm[-1] - current_perm[-2])
+                    # 그 값을 sum_of_val에 갱신
+                    sum_of_val += abs_sub
+
                 backtracking(current_perm, visited, sum_of_val)
+                sum_of_val -= abs_sub
                 current_perm.pop()
                 visited[i] = False
 
     backtracking([], [False] * length_n, 0)
-    print(result)
-    # 순열들의 리스트를 만드는 것 부터 구현했음.
+    return max_val
 
 
 if __name__ == "__main__":
-    perm(X)
+    print(perm(X))
