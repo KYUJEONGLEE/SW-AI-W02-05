3274d822bf86bb2904a0b3c27331517390fef083
이규정 <veracro97@gmail.com>
2026-03-16 13:57:42 +0900

    난이도 중_철도공사, 시간초과 자료구조 바꾸기 전

--- week3/problem-solving/난이도중__철도공사_골드4.py
+++ week3/problem-solving/난이도중__철도공사_골드4.py
@@ -1,10 +1,13 @@
 
 # 문제 링크: https://www.acmicpc.net/problem/23309
+import sys
 
-N, M = map(int, input().split())
-rail = list(map(int, input().split()))
-command = [list(input().rstrip().split()) for _ in range(M)]
-prev, next = {}, {}
+N, M = map(int, sys.stdin.readline().split())
+rail = list(map(int, sys.stdin.readline().split()))
+# command = [list(sys.stdin.readline().rstrip().split()) for _ in range(M)]
+
+prev, next = [], []
+command_line = []
 
 # 입력받은 rail 리스트를 prev, next 의 정보를 담고있는 dictionary 로 가공하는 과정
 for index, num in enumerate(rail):
@@ -12,7 +15,7 @@ for index, num in enumerate(rail):
         prev[num] = rail[-1]
         next[num] = rail[1]
 
-    if index == len(rail) - 1:
+    elif index == len(rail) - 1:
         prev[num] = rail[index - 1]
         next[num] = rail[0]
 
@@ -26,11 +29,13 @@ for index, num in enumerate(rail):
 # next = {2: 7, 7: 3, 3: 5, 5: 2}, prev = {2: 5, 7: 2, 3: 7, 5: 3}
 
 # 이제 입력받은 커맨드들을 하나씩 처리한다.
-for cmd in command:
+for _ in range(M):
+    cmd = sys.stdin.readline().rstrip().split()
     if cmd[0] == "BN":
         cmd[1] = int(cmd[1])
         cmd[2] = int(cmd[2])
-        print(f"{next[cmd[1]]}")
+        # print(f"{next[cmd[1]]}")
+        command_line.append(next[cmd[1]])
         temp_rail = next[cmd[1]]
         # 해당 역의 다음 역을 출력 완료
         # cmd[2]의 역을 cmd[1]의 역 다음으로 설정한다.
@@ -45,8 +50,8 @@ for cmd in command:
         cmd[1] = int(cmd[1])
         cmd[2] = int(cmd[2])
 
-        print(f"{prev[cmd[1]]}")
-
+        # print(f"{prev[cmd[1]]}")
+        command_line.append(prev[cmd[1]])
         temp_rail = prev[cmd[1]]
 
         prev[cmd[1]] = cmd[2]
@@ -59,20 +64,27 @@ for cmd in command:
     elif cmd[0] == "CN":
         cmd[1] = int(cmd[1])
 
-        print(f"{next[cmd[1]]}")
-
+        # print(f"{next[cmd[1]]}")
+        command_line.append(next[cmd[1]])
         temp_rail = next[cmd[1]]
 
         next[cmd[1]] = next[temp_rail]
         prev[next[temp_rail]] = cmd[1]
 
-        pass
+        # del next[temp_rail]
+        # del prev[temp_rail]
+
     elif cmd[0] == "CP":
         cmd[1] = int(cmd[1])
 
-        print(f"{prev[cmd[1]]}")
-
+        # print(f"{prev[cmd[1]]}")
+        command_line.append(prev[cmd[1]])
         temp_rail = prev[cmd[1]]
 
         prev[cmd[1]] = prev[temp_rail]
         next[prev[temp_rail]] = cmd[1]
+
+        # del next[temp_rail]
+        # del prev[temp_rail]
+
+print('\n'.join(map(str, command_line)))
