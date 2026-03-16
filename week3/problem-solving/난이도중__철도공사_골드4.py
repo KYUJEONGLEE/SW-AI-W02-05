3b8ef8924d2364f0ade16607e6a94a2a8e0552cf
이규정 <veracro97@gmail.com>
2026-03-16 13:27:19 +0900

    난이도 중_철도공사, 시간초과

--- week3/problem-solving/난이도중__철도공사_골드4.py
+++ week3/problem-solving/난이도중__철도공사_골드4.py
@@ -1,2 +1,78 @@
 
 # 문제 링크: https://www.acmicpc.net/problem/23309
+
+N, M = map(int, input().split())
+rail = list(map(int, input().split()))
+command = [list(input().rstrip().split()) for _ in range(M)]
+prev, next = {}, {}
+
+# 입력받은 rail 리스트를 prev, next 의 정보를 담고있는 dictionary 로 가공하는 과정
+for index, num in enumerate(rail):
+    if index == 0:
+        prev[num] = rail[-1]
+        next[num] = rail[1]
+
+    if index == len(rail) - 1:
+        prev[num] = rail[index - 1]
+        next[num] = rail[0]
+
+    else:
+        prev[num] = rail[index - 1]
+        next[num] = rail[index + 1]
+
+# 위 과정을 거치면 초기 입력값들이 prev, next에 각각의 다음역, 이전역의 정보를 담고 있는
+# dictionary 로 변환된다.
+# 백준 입력을 예시로 들면
+# next = {2: 7, 7: 3, 3: 5, 5: 2}, prev = {2: 5, 7: 2, 3: 7, 5: 3}
+
+# 이제 입력받은 커맨드들을 하나씩 처리한다.
+for cmd in command:
+    if cmd[0] == "BN":
+        cmd[1] = int(cmd[1])
+        cmd[2] = int(cmd[2])
+        print(f"{next[cmd[1]]}")
+        temp_rail = next[cmd[1]]
+        # 해당 역의 다음 역을 출력 완료
+        # cmd[2]의 역을 cmd[1]의 역 다음으로 설정한다.
+        next[cmd[1]] = cmd[2]
+        prev[temp_rail] = cmd[2]
+        # 그리고 새롭게 추가 된 역의 다음역을 기존 cmd[1] 이 가리키고 있던
+        # 역으로 설정한다.
+        next[cmd[2]] = temp_rail
+        prev[cmd[2]] = cmd[1]
+        # 추가적으로 한개의 역을 삽입햇다고 prev도 같이 변경해줘야한다.
+    elif cmd[0] == "BP":
+        cmd[1] = int(cmd[1])
+        cmd[2] = int(cmd[2])
+
+        print(f"{prev[cmd[1]]}")
+
+        temp_rail = prev[cmd[1]]
+
+        prev[cmd[1]] = cmd[2]
+        next[temp_rail] = cmd[2]
+
+        prev[cmd[2]] = temp_rail
+        next[cmd[2]] = cmd[1]
+
+    # 삭제하는 연산
+    elif cmd[0] == "CN":
+        cmd[1] = int(cmd[1])
+
+        print(f"{next[cmd[1]]}")
+
+        temp_rail = next[cmd[1]]
+
+        next[cmd[1]] = next[temp_rail]
+        prev[next[temp_rail]] = cmd[1]
+
+        pass
+    elif cmd[0] == "CP":
+        cmd[1] = int(cmd[1])
+
+        print(f"{prev[cmd[1]]}")
+
+        temp_rail = prev[cmd[1]]
+
+        prev[cmd[1]] = prev[temp_rail]
+        next[prev[temp_rail]] = cmd[1]
--- week3/problem-solving/난이도중__탑_골드5.py
+++ week3/problem-solving/난이도중__탑_골드5.py
@@ -1,36 +1,14 @@
-
-# 문제 링크: https://www.acmicpc.net/problem/2493
-
 N = int(input())
 Tower = list(map(int, input().split()))
 
 compare_stack = []
 answer = [0] * N
-stack_count = 0
 
-# for tower in Tower[::-1]:
-#     compare_stack.append(tower)
-#     Tower.pop()
-
-#     for tower2 in Tower[::-1]:
-#         if not compare_stack:
-#             break
-
-#         if tower2 > tower:
-#             while compare_stack:
-#                 compare_stack.pop()
-#                 answer.append(stack_count)
-#         else:
-#             Tower.pop()
-#             compare_stack.append(tower2)
 origin_tower = len(Tower)
 while Tower:
-    # 입력 Tower 리스트가 존재하면
-    # 비교스택이 비어있으면 새로 원소 추가
     if not compare_stack:
         compare_stack.append((len(Tower), Tower[-1]))
         Tower.pop()
-    # 비교스택이 비어있지 않으면, 즉 비교해야 하는 상황이 있으면
     else:
         if compare_stack[-1][1] > Tower[-1]:
             compare_stack.append((len(Tower), Tower[-1]))
