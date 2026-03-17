e124a798b50a1921748708314d93e21eb8347240
이규정 <veracro97@gmail.com>
2026-03-17 11:37:19 +0900

    solve_난이도 중_뱀

--- week3/problem-solving/난이도중__뱀_골드4.py
+++ week3/problem-solving/난이도중__뱀_골드4.py
@@ -10,18 +10,6 @@
 
 반복문은 while 로 검사해서 충돌 조건(bool)로 빠져나오는건가
 큐?
-[1 0 0 0 0 0] 이고 왼쪽으로 0 이 삽입된다고 생각해보자
-[0 1 0 0 0 0] 이 된다. 3번쨰 칸에서 사과를 먹었다고 하면
-[0 1 1 0 0 0]
-
-[0 0 0 0 0 0]
-[0 0 0 0 1 0]
-[0 0 0 1 0 0]
-[0 0 0 0 0 0]
-[0 0 1 0 0 0]
-[0 0 0 0 0 0]
-
-위는 사과 위치를 1로 표현한 2차원 배열의 보드판이다.
 
 vector? (1,0)(0,1)(-1,0)(0,-1)
 """
@@ -34,53 +22,93 @@ apple_location = [tuple(map(int, sys.stdin.readline().split())
                         )for _ in range(K)]
 apple_location = dict.fromkeys(apple_location, 1)
 L = int(sys.stdin.readline())
-shift = [sys.stdin.readline().split() for _ in range(L)]
+Command = [sys.stdin.readline().split() for _ in range(L)]
 
 
 snake_queue = deque([])
 x, y = 1, 1
 
 vector = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-right, left, up, down = 0, 1, 2, 3
 game_over = False
-face = -1
+face = 0
 count = 0
-# if not snake_queue:
-#     snake_queue.append((x, y))
+
+if not snake_queue:
+    snake_queue.append((x, y))
 # snake가 비어있다면 생성
 # 일단 처음에는 무조건 오른쪽으로 이동함.
-for cmd in shift:
+
+
+for cmd in Command:
 
     second = int(cmd[0]) - count
     direction = cmd[1]
 
-    if direction == "D":
-        face += 1
-    elif direction == "L":
-        face -= 1
-
     while second > 0:
+
         count += 1
         x += vector[face][0]
         y += vector[face][1]
 
+        if (x, y) in snake_queue:
+            game_over = True
+            break
+
+        if x > N or y > N or x <= 0 or y <= 0:  # 부딪힌다면
+            game_over = True
+            break
+
         snake_queue.append((x, y))
 
         if snake_queue[-1] in apple_location:  # 사과가 있다면
+            if apple_location[snake_queue[-1]] == 0:
+                snake_queue.popleft()
+
             apple_location[snake_queue[-1]] = 0
+
         elif snake_queue[-1] not in apple_location:  # 사과가 없다면
             snake_queue.popleft()
 
-        if x > N or y > N or x < 0 or y < 0:  # 부딪힌다면
-            game_over = True
-            break
-        if (x + vector[face][0], y + vector[face][1]) in snake_queue:
-            game_over = True
-            break
-
         second -= 1
 
+    if direction == "D":
+        face = (face + 1) % 4
+    elif direction == "L":
+        face = (face - 1) % 4
     if game_over:
         break
+
+# face = 2
+# direction = 'L'
+# count = 13
+# K =4
+# L =4
+# que = deque([(2, 9), (3, 9), (3, 8), (2, 8), (1, 8)])
+# x = 1, y = 8
+# game_over = False
+while not game_over:
+    count += 1
+
+    x += vector[face][0]
+    y += vector[face][1]
+
+    if (x, y) in snake_queue:
+        game_over = True
+        break
+
+    if x > N or y > N or x <= 0 or y <= 0:  # 부딪힌다면
+        game_over = True
+        break
+
+    snake_queue.append((x, y))
+
+    if snake_queue[-1] in apple_location:  # 사과가 있다면
+        if apple_location[snake_queue[-1]] == 0:
+            snake_queue.popleft()
+
+        apple_location[snake_queue[-1]] = 0
+
+    elif snake_queue[-1] not in apple_location:  # 사과가 없다면
+        snake_queue.popleft()
 
 print(count)
