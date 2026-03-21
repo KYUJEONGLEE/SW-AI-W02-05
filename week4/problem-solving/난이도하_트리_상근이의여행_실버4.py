b054ff3eed1170df28e8a3014c2cb8b98342e065
이규정 <veracro97@gmail.com>
2026-03-21 15:47:03 +0900

    solve_난이도하_트리_상근이의 여행, 트리만들기

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,28 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+"""
+문제 접근 방법:
+
+해결 방법:
+
+"""
+import sys
+
+
+def create_graph(vertices, edges):
+    graph = {i: [] for i in range(1, vertices + 1)}
+    for u, v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+
+    return graph
+
+
+if __name__ == "__main__":
+    T = int(sys.stdin.readline())
+    for _ in range(T):
+        N, M = sys.stdin.readline().split()
+        edge = list(tuple(map(int, sys.stdin.readline().split()))
+                    for _ in range(int(M)))
+
+        print(int(N)-1)
--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,14 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+import sys
+
+N, M = sys.stdin.readline().split()
+N, M = int(N), int(M)
+
+# N 개의 정점, M개의 리프노드(차수가 1인 = 본인한테 연결되어 있는 간선의 수가 1)
+
+for i in range(N - 1):
+    if i < N - M + 1:
+        print(f"{i} {i + 1}")
+    else:
+        print(f"{N - M} {i + 1}")
