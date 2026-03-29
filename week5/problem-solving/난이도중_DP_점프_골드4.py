abec0179bba3281c06abf04a8e34890df05f0a78
이규정 <veracro97@gmail.com>
2026-03-30 01:59:05 +0900

    solve_신입사원

--- week5/problem-solving/난이도중_DP_점프_골드4.py
+++ week5/problem-solving/난이도중_DP_점프_골드4.py
@@ -1,2 +1,12 @@
 # DP - 점프 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2253
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+x_stone = []
+
+for _ in range(M):
+    m = int(sys.stdin.readline())
+    x_stone.append(m)
--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -7,7 +7,35 @@ T = int(sys.stdin.readline())
 
 for _ in range(T):
     rank = []
+    selected = set()
+    count = 1
     N = int(sys.stdin.readline())
     for _ in range(N):
         x, y = map(int, sys.stdin.readline().split())
         rank.append((x, y))
+
+    if len(rank) == 1:
+        print(len(rank))
+    else:
+        doc = sorted(rank, key=lambda x: x[0])
+        interview = sorted(rank, key=lambda x: x[1])
+
+        min_doc = doc[0]
+        min_inter = interview[0]
+        # doc = min(rank, key=lambda x: x[0])
+        # interview = min(rank, key=lambda x: x[1])
+
+        selected.add(min_doc)
+        selected.add(min_inter)
+
+        for a, b in doc:
+            if b < min_doc[1]:
+                min_doc = (a, b)
+                selected.add(min_doc)
+
+        for a, b in interview:
+            if (a, b) in selected and a < min_inter[0]:
+                min_inter = (a, b)
+                count += 1
+
+        print(count)
